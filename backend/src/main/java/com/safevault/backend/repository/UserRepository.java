0a55db7b44e29cb7f3b7d1791a5d591b48615d23
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-19 17:38:18 +0530

    add optional

--- backend/src/main/java/com/safevault/backend/repository/UserRepository.java
+++ backend/src/main/java/com/safevault/backend/repository/UserRepository.java
@@ -7,10 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.safevault.backend.model.User;
+import java.util.Optional;
 
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
