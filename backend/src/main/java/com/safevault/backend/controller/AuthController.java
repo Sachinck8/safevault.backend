cbfc445fea16b3f3ca567a4cf86aae7862dfaad1
Sachinck8 <130655708+Sachinck8@users.noreply.github.com>
2025-11-19 17:27:10 +0530

    fix some import

--- backend/src/main/java/com/safevault/backend/controller/AuthController.java
+++ backend/src/main/java/com/safevault/backend/controller/AuthController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.safevault.backend.service.UserService;
+
 @Controller
 public class AuthController {
 
